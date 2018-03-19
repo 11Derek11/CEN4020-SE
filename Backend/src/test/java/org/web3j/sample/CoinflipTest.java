d2b40678d3b54fc5dbbd7e4855eafd698f4faa2d
shak582 <shak582@hotmail.com>
2018-03-19 18:07:55 -0400

    decoding the string to check the message
    we would check the bytes themselves, which was prone to error. Now it gets parsed.

--- Backend/src/test/java/org/web3j/sample/CoinflipTest.java
+++ Backend/src/test/java/org/web3j/sample/CoinflipTest.java
@@ -6,9 +6,10 @@ import org.junit.Test;
 import java.lang.Exception;
 import java.io.IOException;
 
-
+import javax.xml.bind.DatatypeConverter;
 import java.lang.String;
 import org.web3j.backend.CoinFlip;
+import org.web3j.backend.Application;
 import java.util.Arrays;
 import java.nio.ByteBuffer;
 import org.web3j.crypto.Credentials;
@@ -31,13 +32,12 @@ public class CoinflipTest{
 	@Test
 	public void testCoinflip()
 	{
-		Web3j web3 = Web3j.build(new HttpService());
-
 		try{
-			String lose = "0x0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000f8ef3cd122bedb0648d18ab224601ead6eec2fab000000000000000000000000000000000000000000000000f9ccd8a1c5080000000000000000000000000000000000000000000000000000000000000000000b796f75206c6f7365212121000000000000000000000000000000000000000000";
-			String win = 	"0x0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000f8ef3cd122bedb0648d18ab224601ead6eec2fab000000000000000000000000000000000000000000000000f9ccd8a1c5080000000000000000000000000000000000000000000000000000000000000000000a796f7520776f6e21212100000000000000000000000000000000000000000000";
-
+			Web3j web3 = Web3j.build(new HttpService());
+			String lose = "you lose!!!";
+			String win = "you won!!!";
 			String check = "";
+			byte[] mesg;
 
 			Web3ClientVersion clientVersion = web3.web3ClientVersion().sendAsync().get();
 
@@ -49,13 +49,13 @@ public class CoinflipTest{
 				System.out.println("Client is running version: " + clientVersion.getWeb3ClientVersion());
 			}
 
-			Credentials credentials = WalletUtils.loadCredentials("sapha582", "/home/shak/.ethereum/rinkeby/keystore/UTC--2018-02-15T03-29-56.001144798Z--f8ef3cd122bedb0648d18ab224601ead6eec2fab");
+			Credentials credentials = WalletUtils.loadCredentials("", "/home/shak/.ethereum/rinkeby/keystore/UTC--2018-02-15T03-29-56.001144798Z--f8ef3cd122bedb0648d18ab224601ead6eec2fab");
 		
 
 			System.out.println("Credentials Loaded.");
 			CoinFlip contract = CoinFlip.load("0xbf77E081F2fA4F51adDB19b43f8E870AAa1714e8",web3,credentials,ManagedTransaction.GAS_PRICE,Contract.GAS_LIMIT);
 
-			TransactionReceipt trans = contract.playCoinFlip("tails", initial.multiply(BigInteger.valueOf(20))).send();
+			TransactionReceipt trans = contract.playCoinFlip("heads", initial.multiply(BigInteger.valueOf(1))).send();
 
 			for(Log log : trans.getLogs())
 			{
@@ -63,9 +63,15 @@ public class CoinflipTest{
 				System.out.println(log.getData());
 			}
 
-			assertTrue(check.equals(win) || check.equals(lose));
+			mesg = DatatypeConverter.parseHexBinary(check.substring(2));
+			check = new String(mesg);
+
+
+			System.out.println(check);
+			assertTrue(check.contains(win) || check.contains(lose));
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 }
